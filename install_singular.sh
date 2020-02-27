3d6741e673e46f1feda44a51fbc424084456b7cb
user <shouldsee@qq.com>
2020-02-27 20:34:03 +0800

    ci

--- install_singular.sh
+++ install_singular.sh
@@ -14,14 +14,13 @@ _down(){
 export VERSION=1.13.5 OS=linux ARCH=amd64 
 _down -o go-$VERSION.tar.gz https://dl.google.com/go/go$VERSION.$OS-$ARCH.tar.gz
 tar -C /usr/local/ -xzf go-$VERSION.tar.gz
-ln -sf /usr/local/go/bin/go /usr/local/bin/go
+# ln -sf /usr/local/go/bin/go /usr/local/bin/go
+echo 'export GOPATH=${HOME}/go' >> ~/.bashrc 
+echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc 
+source ~/.bashrc    
 go --help
 echo "[installed] go"
-
-# echo 'export GOPATH=${HOME}/go' >> ~/.bashrc && \
-#     echo 'export PATH=/usr/local/go/bin:${PATH}:${GOPATH}/bin' >> ~/.bashrc && \
-#     source ~/.bashrc    
-
+	
 VERSION=3.5.3
 URL=https://github.com/singularityware/singularity/releases/download/v$VERSION/singularity-$VERSION.tar.gz
 _down -o $(basename $URL) $URL
