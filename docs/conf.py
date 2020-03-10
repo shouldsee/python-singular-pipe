d5af63bcc14e937c0422b3e49250f1d5275de692
shouldsee <shouldsee@github.com>
2020-03-10 16:05:12 +0800

    docs

--- docs/conf.py
+++ docs/conf.py
@@ -50,7 +50,8 @@ exclude_patterns = ['_build', 'Thumbs.db', '.DS_Store']
 # a list of builtin themes.
 #
 #html_theme = 'alabaster'
-html_theme = 'classic'
+# html_theme = 'classic'
+html_theme = 'sphinx_rtd_theme'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
@@ -68,13 +69,14 @@ html_theme_options = {
     # 'prev_next_buttons_location': 'bottom',
     # 'style_external_links': False,
     # 'vcs_pageview_mode': '',
-    # 'style_nav_header_background': 'white',
-    # # Toc options
-    'collapse_navigation': True,
-    'sticky_navigation': True,
-    'navigation_depth': 4,
-    'includehidden': True,
-    'titles_only': False
+    # # 'style_nav_header_background': 'white',
+
+    # # # Toc options
+    # 'collapse_navigation': True,
+    # 'sticky_navigation': True,
+    # 'navigation_depth': 4,
+    # 'includehidden': True,
+    # 'titles_only': False
 }
 # sys.path.insert(0, os.path.join(os.path.realpath(os.path.dirname(__file__)),'singular_pipe'))
 # sys.path.insert(0, os.path.dirname(__file__),)
