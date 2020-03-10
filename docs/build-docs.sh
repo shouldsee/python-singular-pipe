42fa73a831b699a7ed03b8956b99bb0b21ce75d5
shouldsee <shouldsee@github.com>
2020-03-10 14:50:08 +0800

    u

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -1,7 +1,8 @@
 set -e
 ### install package first
+cp -rfT docs _docs
 git checkout gh-pages
-sphinx-build -b html docs _html
+sphinx-build -b html _docs _html
 cp -lfrv _html/* -t .
 git add .; git commit . -m "$@"; git push origin gh-pages
 
