13673c06d22287ce868877dbf9924c8cd17e3356
shouldsee <shouldsee@github.com>
2020-03-10 15:39:21 +0800

    docs

--- docs/build-docs.sh
+++ docs/build-docs.sh
@@ -5,7 +5,7 @@ rm -rf ./_docs
 cp -rfT docs _docs
 git checkout gh-pages
 sphinx-build -b html _docs _html
-cp -lfrv _html/* -t .
+cp -lfr _html/* -t .
 git add .; git commit . -m docs; git push origin gh-pages
 git checkout master -f
 #mv html/* .
